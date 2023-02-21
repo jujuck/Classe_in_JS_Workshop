8796452b0553539b36b9fa17043cb857d74934bb
Julien RICHARD <julien.y.richard@gmail.com>
2023-02-21 10:02:01 +0100

    Creating the repo for the Classe JS Workshop

---
+++ src/Motorcycle.js
@@ -0,0 +1,5 @@
+class Motorcycle {
+  // TODO
+}
+
+export default Motorcycle;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const myVehicle = new Vehicle("Monocycle", "red", 1);
+// myVehicle.logTypeAndProps();
+
+const myCar = new Car("DeLorean DMC-12", "silver");
+// myCar.logTypeAndProps();
+
+const myMotorcyle = new Motorcycle("Kawasa­ki ZZR 250", "yellow");
+// myMotorcyle.logTypeAndProps();
